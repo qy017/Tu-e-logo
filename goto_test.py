3800ed0199edc0e9d4443d21fee31525731e6f04
qy017 <148801018+qy017@users.noreply.github.com>
2024-04-29 18:47:44 +0200

    Update goto_test.py

--- goto_test.py
+++ goto_test.py
@@ -3,7 +3,7 @@ from pycrazyswarm import *
 from pycrazyswarm import Crazyswarm
 from numpy.linalg import norm
 
-V=4.0
+V=2.0
 
 if __name__ == "__main__":
     swarm = Crazyswarm()
@@ -15,14 +15,19 @@ if __name__ == "__main__":
     # timeHelper.sleep(2.0)
 
 
-    cf0.goTo(np.array([0,0,1]),0,V)
-    timeHelper.sleep(1.0)
-    current_pos = cf0.position()
-    cf0.goTo(np.array(current_pos),0,V)
-    timeHelper.sleep(1.0)
-    cf0.goTo(np.array([0,0,1]),0,V)
+    while True:
+        current_pos = cf0.position()
+        if ((norm(np.array(current_pos)-np.array([0,0,0.5]),2)>0.01)):
+            cf0.goTo(np.array([0,0,1]),0,V)
+        else:
+            cf0.goTo(np.array(current_pos),0,0)
+            timeHelper.sleep(2.0)
+            cf0.goTo(np.array([0,0,1]),0,V)
+            timeHelper.sleep(4.0)
+            allcfs.land(targetHeight=0.02, duration=3.0)
+            timeHelper.sleep(3.0)
 
-    print("press button to continue...")
-    swarm.input.waitUntilButtonPressed()
-    allcfs.land(targetHeight=0.02, duration=3.0)
-    timeHelper.sleep(3.0)
+    # print("press button to continue...")
+    # swarm.input.waitUntilButtonPressed()
+    # allcfs.land(targetHeight=0.02, duration=3.0)
+    # timeHelper.sleep(3.0)
