eabb6b1aee687bd521ee1e467a35b09cf8e50e31
qy017 <148801018+qy017@users.noreply.github.com>
2024-04-29 17:46:34 +0200

    Add files via upload

---
+++ goto_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+from pycrazyswarm import *
+from pycrazyswarm import Crazyswarm
+from numpy.linalg import norm
+
+V=4.0
+
+if __name__ == "__main__":
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+    cf0 = swarm.allcfs.crazyflies[0]
+
+    # allcfs.takeoff(targetHeight=0.2, duration=2.0)
+    # timeHelper.sleep(2.0)
+
+
+    cf0.goTo(np.array([0,0,1]),0,V)
+    timeHelper.sleep(1.0)
+    current_pos = cf0.position()
+    cf0.goTo(np.array(current_pos),0,V)
+    timeHelper.sleep(1.0)
+    cf0.goTo(np.array([0,0,1]),0,V)
+
+    print("press button to continue...")
+    swarm.input.waitUntilButtonPressed()
+    allcfs.land(targetHeight=0.02, duration=3.0)
+    timeHelper.sleep(3.0)
