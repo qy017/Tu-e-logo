523dde638cef5f447d55b3065a22b5a929053b24
Briana2402 <brianaisaila@gmail.com>
2024-05-03 17:24:19 +0200

    TUe complete demo

--- inclinedTUeShape.py
+++ inclinedTUeShape.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     cf4 = swarm.allcfs.crazyflies[3]
     cf5 = swarm.allcfs.crazyflies[4]
     cf6 = swarm.allcfs.crazyflies[5]
+    cf7 = swarm.allcfs.crazyflies[6]
     
     allcfs.takeoff(targetHeight=0.2, duration=2.0)
     timeHelper.sleep(2.0)
@@ -54,6 +55,25 @@ if __name__ == "__main__":
     cf6.goTo(np.array([0.3,-0.4,0.2]),0,V)
     timeHelper.sleep(pause)
 
+    print("press button to continue...")
+    swarm.input.waitUntilButtonPressed()
+    pause = 1.0;
+
+    #inclined e-shape:
+    cf1.goTo(np.array([-0.7,-0.2,1.1]),0,V)
+    timeHelper.sleep(pause)
+    cf2.goTo(np.array([-0.4,0,1.7]),0,V)
+    timeHelper.sleep(pause)
+    cf3.goTo(np.array([0.4,0,1.7]),0,V)
+    timeHelper.sleep(pause)
+    cf4.goTo(np.array([0.7,-0.2,1.1]),0,V)
+    timeHelper.sleep(pause)
+    cf5.goTo(np.array([-0.55,-0.4,0.6]),0,V)
+    timeHelper.sleep(pause)
+    cf6.goTo(np.array([0,-0.2,1.05]),0,V)
+    timeHelper.sleep(pause)
+    cf6.goTo(np.array([0,-0.6,0.4]),0,V)
+    timeHelper.sleep(pause)
 
     print("press button to continue...")
     swarm.input.waitUntilButtonPressed()
