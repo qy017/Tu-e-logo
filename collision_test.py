34356a25b80f245168e5134d179613e65855ddc7
qy017 <148801018+qy017@users.noreply.github.com>
2024-05-03 19:59:28 +0200

    Update collision_test.py

--- collision_test.py
+++ collision_test.py
@@ -3,37 +3,47 @@ from pycrazyswarm import *
 from pycrazyswarm import Crazyswarm
 from numpy.linalg import norm
 
-V=2.0
-pos=[]
-cf=[0,0,0,0,0]
-safe_distance =0.5
+V=5.0
+
+safe_distance =0.15
 
 if __name__ == "__main__":
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
-    cf[0] = swarm.allcfs.crazyflies[0]
-    cf[1] = swarm.allcfs.crazyflies[1]
+    cf0 = swarm.allcfs.crazyflies[0]
+    cf1 = swarm.allcfs.crazyflies[1]
 
-    allcfs.takeoff(targetHeight=0.2, duration=2.0)
-    timeHelper.sleep(2.0)
+    # allcfs.takeoff(targetHeight=0.2, duration=2.0)
+    # timeHelper.sleep(2.0)
 
-    while True:
-        cf[0].goTo(np.array([0,0,1]),0,V)
-        cf[1].goTo(np.array([-1,0,1]),0,V)
-        pos[0] = cf[0].position()
-        pos[1] = cf[1].position()
-        d = norm(pos[0]-pos[1],2)
-        if d<safe_distance:
-            current_position = cf[0].position()
-            cf[0].goTo(np.array(current_position),0,V) #one drone stays in current position while the others go
+    while (True):
+        # cf[0].goTo(np.array([0,0,1]),0,V)
+        # cf[1].goTo(np.array([-1,0,1]),0,V)
+        pos0 = cf0.position()
+        pos1 = cf1.position()
+        current_pos = cf0.position()
+        # d = norm(pos0-pos1,2)
+        # print(d)
+        # land_d0=norm(np.array(pos0)-np.array([0,0,1]),2)
+        # land_d1=norm(np.array(pos1)-np.array([-1,0,1]),2)
+        if ((norm(np.array(pos0)-np.array(pos1),2))>safe_distance):
+            # current_position = cf0.position()
+            cf0.goTo(np.array([0,0,1]),0,V)
+            cf1.goTo(np.array([-1,0,1]),0,V)
+            current_pos = cf0.position()
+            # timeHelper.sleep(3.0)
+        else:
+            current_pos = cf0.position()
+            cf0.goTo(np.array(current_pos),0,0)
             timeHelper.sleep(3.0)
-        if ((pos[0]==[0,0,1])&(pos[1]==[-1,0,1])):
+        if ((norm(np.array(pos0)-np.array([0,0,1]),2)<0.02)|(norm(np.array(pos1)-np.array([-1,0,1]),2)<0.02)):
+            print("press button to continue...")
+            swarm.input.waitUntilButtonPressed()
+            allcfs.land(targetHeight=0.02, duration=3.0)
+            timeHelper.sleep(2.0)
             break
-
-    print("press button to continue...")
-    swarm.input.waitUntilButtonPressed()
-    allcfs.land(targetHeight=0.02, duration=1.0)
-    timeHelper.sleep(2.0)
+        
+    
 
 
