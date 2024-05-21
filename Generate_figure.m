b792caf2394f216e774cf1ad0274ced02bf3ad16
qy017 <148801018+qy017@users.noreply.github.com>
2024-05-21 18:00:40 +0200

    Update Generate_figure.m

--- Generate_figure.m
+++ Generate_figure.m
@@ -3,29 +3,35 @@ close
 clc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Goal: Generate different figures
-%Author: Ming
-%Date: May 26th, 2022
+%Author: Qianyu
+%Date: May 21st, 2024
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Generate Waypoints of Figure 6
-T = [-0.7,0,2.0; 
-    0,0,2.0; 
-    0.7,0,2.0; 
-    0,-0.2,1.4; 
-    0,-0.4,0.8; 
+% T = [-0.7,0,2.0; 
+%     0,0,2.0; 
+%     0.7,0,2.0; 
+%     0,-0.2,1.4; 
+%     0,-0.4,0.8; 
+%     0,-0.6, 0.2];
+T = [-0.7,0,1.7; 
+    0,0,1.7; 
+    0.7,0,1.7; 
+    0,-0.2,1.2; 
+    0,-0.4,0.7; 
     0,-0.6, 0.2];
-U = [-0.7,-0.2,0.9;
-    -0.7,0,1.8;
-    0.6,0,1.8;
-    0.6,-0.2,0.9;
-    -0.3,-0.4,0.2;
-    0.3,-0.4,0.2];
-E = [-0.7,-0.2,1.1;
-    -0.4,0,1.7;
-    0.4,0,1.7;
-    0.7,-0.2,1.1;
-    -0.55,-0.4,0.6;
-    0,-0.2,1.05;
-    0,-0.6,0.4];
+% U = [-0.7,-0.2,0.9;
+%     -0.7,0,1.8;
+%     0.6,0,1.8;
+%     0.6,-0.2,0.9;
+%     -0.3,-0.4,0.2;
+%     0.3,-0.4,0.2];
+% E = [-0.7,-0.2,1.1;
+%     -0.4,0,1.7;
+%     0.4,0,1.7;
+%     0.7,-0.2,1.1;
+%     -0.55,-0.4,0.6;
+%     0,-0.2,1.05;
+%     0,-0.6,0.4];
 initial_pos = [
     -0.5,1,0;
     0.5,1,0;
@@ -34,21 +40,26 @@ initial_pos = [
     1,0,0;
     -0.5,-1,0;
     0.5,-1,0];
+mid1 = [(T(1,1)+initial_pos(1,1))/2, (T(1,2)+initial_pos(1,2))/2, (T(1,3)+initial_pos(1,3))/2];
+mid2 = [(T(2,1)+initial_pos(2,1))/2, (T(2,2)+initial_pos(2,2))/2, (T(2,3)+initial_pos(2,3))/2];
+mid3 = [(T(3,1)+initial_pos(3,1))/2, (T(3,2)+initial_pos(3,2))/2, (T(3,3)+initial_pos(3,3))/2];
+mid4 = [(T(4,1)+initial_pos(4,1))/2, (T(4,2)+initial_pos(4,2))/2, (T(4,3)+initial_pos(4,3))/2];
+mid5 = [(T(5,1)+initial_pos(5,1))/2, (T(5,2)+initial_pos(5,2))/2, (T(5,3)+initial_pos(5,3))/2];
+mid6 = [(T(6,1)+initial_pos(6,1))/2, (T(6,2)+initial_pos(6,2))/2, (T(6,3)+initial_pos(6,3))/2];
 
-traj_T1 = [initial_pos(1,:);T(1,:)];
-traj_T2 = [initial_pos(2,:);T(2,:)];
-traj_T3 = [initial_pos(3,:);T(3,:)];
-traj_T4 = [initial_pos(4,:);T(4,:)];
-traj_T5 = [initial_pos(5,:);T(5,:)];
-traj_T6 = [initial_pos(6,:);T(6,:)];
-
-figure(1);
-plot3(T(:,1),T(:,2),T(:,3),'*');
-hold on;
-plot3(U(:,1),U(:,2),U(:,3),'*');
-plot3(E(:,1),E(:,2),E(:,3),'*');
-plot3(initial_pos(:,1),initial_pos(:,2),initial_pos(:,3),'*');
-grid on;
+traj_T1 = [initial_pos(1,:);mid1; T(1,:)];
+traj_T2 = [initial_pos(2,:);mid2;T(2,:)];
+traj_T3 = [initial_pos(3,:);mid3;T(3,:)];
+traj_T4 = [initial_pos(4,:);mid4;T(4,:)];
+traj_T5 = [initial_pos(5,:);mid5;T(5,:)];
+traj_T6 = [initial_pos(6,:);mid6;T(6,:)];
+% figure(1);
+% plot3(T(:,1),T(:,2),T(:,3),'*');
+% hold on;
+% plot3(U(:,1),U(:,2),U(:,3),'*');
+% plot3(E(:,1),E(:,2),E(:,3),'*');
+% plot3(initial_pos(:,1),initial_pos(:,2),initial_pos(:,3),'*');
+% grid on;
 
 figure(2);
 grid on;
@@ -70,7 +81,7 @@ waypoints_orientation6 =zeros(size(traj_T6,1),3);
 
 % Time_interval=[1.9273,1.2,0.5,0.7654,0.7654,0.7654,0.7654,0.7654,0.7654,0.5,1];     %Figure 6 time interval
 % Time_interval=[1.05,0.71,0.62,0.7,0.56,0.56,0.7,0.62,0.71,1.05,1.05];   %Figure 8 time interval
-Time_T = [3,3];
+Time_T = [3,3,3];
 
 for i=1:length(Time_T)-1
     Time_of_Arrival(i+1)=Time_T(i+1)+sum(Time_T(1:i));
@@ -127,15 +138,33 @@ orientation_coefficients_yaw6=pp6.coefs(4:6:end,8:-1:1) ;
 
 figure(3)
 hold on;
-plot3(q1(1,:),q1(2,:),q1(3,:));
-plot3(q2(1,:),q2(2,:),q2(3,:));
-plot3(q3(1,:),q3(2,:),q3(3,:));
-plot3(q4(1,:),q4(2,:),q4(3,:));
-plot3(q5(1,:),q5(2,:),q5(3,:));
-plot3(q6(1,:),q6(2,:),q6(3,:));
+plot3(q1(1,:),q1(2,:),q1(3,:),'*');
+plot3(q2(1,:),q2(2,:),q2(3,:),'*');
+plot3(q3(1,:),q3(2,:),q3(3,:),'*');
+plot3(q4(1,:),q4(2,:),q4(3,:),'*');
+plot3(q5(1,:),q5(2,:),q5(3,:),'*');
+plot3(q6(1,:),q6(2,:),q6(3,:),'*');
+% plot3(traj_T1(:,1),traj_T1(:,2),traj_T1(:,3),'r*-');
+
 grid on
 
 %% Save coefficients
 % fid = fopen( 'T_1.csv', 'w' );
-Coeffients=[duration_time(1:end-1).',position_coefficients_x1,position_coefficients_y1,position_coefficients_z1,orientation_coefficients_yaw1];
-Create_coefficients_table(Coeffients);
+Coeffients1=[duration_time(1:end-1).',position_coefficients_x1,position_coefficients_y1,position_coefficients_z1,orientation_coefficients_yaw1];
+Create_coefficients_table(Coeffients1,'T_1.csv');
+
+Coeffients2=[duration_time(1:end-1).',position_coefficients_x2,position_coefficients_y2,position_coefficients_z2,orientation_coefficients_yaw2];
+Create_coefficients_table(Coeffients2,'T_2.csv');
+
+Coeffients3=[duration_time(1:end-1).',position_coefficients_x3,position_coefficients_y3,position_coefficients_z3,orientation_coefficients_yaw3];
+Create_coefficients_table(Coeffients3,'T_3.csv');
+
+Coeffients4=[duration_time(1:end-1).',position_coefficients_x4,position_coefficients_y4,position_coefficients_z4,orientation_coefficients_yaw4];
+Create_coefficients_table(Coeffients4,'T_4.csv');
+
+Coeffients5=[duration_time(1:end-1).',position_coefficients_x5,position_coefficients_y5,position_coefficients_z5,orientation_coefficients_yaw5];
+Create_coefficients_table(Coeffients5,'T_5.csv');
+
+Coeffients6=[duration_time(1:end-1).',position_coefficients_x6,position_coefficients_y6,position_coefficients_z6,orientation_coefficients_yaw6];
+Create_coefficients_table(Coeffients6,'T_6.csv');
+
