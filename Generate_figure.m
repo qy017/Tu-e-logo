920f0766ad0f4543f4b5c100400cc49dd5a04224
qy017 <148801018+qy017@users.noreply.github.com>
2024-05-22 16:44:17 +0200

    Update Generate_figure.m
    Trajectories from T to U to e

--- Generate_figure.m
+++ Generate_figure.m
@@ -4,34 +4,50 @@ clc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Goal: Generate different figures
 %Author: Qianyu
-%Date: May 21st, 2024
+%Date: May 22nd, 2024
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Generate Waypoints of Figure 6
-% T = [-0.7,0,2.0; 
-%     0,0,2.0; 
-%     0.7,0,2.0; 
-%     0,-0.2,1.4; 
-%     0,-0.4,0.8; 
-%     0,-0.6, 0.2];
+%% Generate Waypoints of TU/e logo
+%% 18 drones
+% T = [-1.9,0,1.7;
+%     -2.6,0,1.7;
+%     -1.2,0,1.7;
+%     -1.9,-0.2,1.2;
+%     -1.9,-0.4,0.7;
+%     -1.9,-0.6,0.2];
+% U = [-0.7,-0.2,0.9;
+%     -0.7,0,1.7;
+%     0.6,0,1.7;
+%     0.6,-0.2,0.9;
+%     -0.3,-0.4,0.2;
+%     0.3,-0.4,0.2];
+% E = [1.2,-0.2,0.9;
+%     1.5,0,1.5;
+%     2.3,0,1.5;
+%     1.35,-0.4,0.4;
+%     1.9,-0.2,0.85;
+%     1.9,-0.6,0.2;
+%     2.6,-0.2,0.9];
+
+%% Separate:
 T = [-0.7,0,1.7; 
     0,0,1.7; 
     0.7,0,1.7; 
     0,-0.2,1.2; 
     0,-0.4,0.7; 
     0,-0.6, 0.2];
-% U = [-0.7,-0.2,0.9;
-%     -0.7,0,1.8;
-%     0.6,0,1.8;
-%     0.6,-0.2,0.9;
-%     -0.3,-0.4,0.2;
-%     0.3,-0.4,0.2];
-% E = [-0.7,-0.2,1.1;
-%     -0.4,0,1.7;
-%     0.4,0,1.7;
-%     0.7,-0.2,1.1;
-%     -0.55,-0.4,0.6;
-%     0,-0.2,1.05;
-%     0,-0.6,0.4];
+U = [-0.7,-0.2,0.9;
+    -0.7,0,1.7;
+    0.6,0,1.7;
+    0.6,-0.2,0.9;
+    -0.3,-0.4,0.2;
+    0.3,-0.4,0.2];
+E = [-0.7,-0.2,0.9;
+    -0.4,0,1.5;
+    0.4,0,1.5;
+    0.7,-0.2,0.9;
+    -0.55,-0.4,0.4;
+    0,-0.2,0.85;
+    0,-0.6,0.2];
 initial_pos = [
     -0.5,1,0;
     0.5,1,0;
@@ -40,45 +56,57 @@ initial_pos = [
     1,0,0;
     -0.5,-1,0;
     0.5,-1,0];
-mid1 = [(T(1,1)+initial_pos(1,1))/2, (T(1,2)+initial_pos(1,2))/2, (T(1,3)+initial_pos(1,3))/2];
-mid2 = [(T(2,1)+initial_pos(2,1))/2, (T(2,2)+initial_pos(2,2))/2, (T(2,3)+initial_pos(2,3))/2];
-mid3 = [(T(3,1)+initial_pos(3,1))/2, (T(3,2)+initial_pos(3,2))/2, (T(3,3)+initial_pos(3,3))/2];
-mid4 = [(T(4,1)+initial_pos(4,1))/2, (T(4,2)+initial_pos(4,2))/2, (T(4,3)+initial_pos(4,3))/2];
-mid5 = [(T(5,1)+initial_pos(5,1))/2, (T(5,2)+initial_pos(5,2))/2, (T(5,3)+initial_pos(5,3))/2];
-mid6 = [(T(6,1)+initial_pos(6,1))/2, (T(6,2)+initial_pos(6,2))/2, (T(6,3)+initial_pos(6,3))/2];
 
-traj_T1 = [initial_pos(1,:);mid1; T(1,:)];
-traj_T2 = [initial_pos(2,:);mid2;T(2,:)];
-traj_T3 = [initial_pos(3,:);mid3;T(3,:)];
-traj_T4 = [initial_pos(4,:);mid4;T(4,:)];
-traj_T5 = [initial_pos(5,:);mid5;T(5,:)];
-traj_T6 = [initial_pos(6,:);mid6;T(6,:)];
+% Target position plot:
 % figure(1);
-% plot3(T(:,1),T(:,2),T(:,3),'*');
 % hold on;
+% plot3(T(:,1),T(:,2),T(:,3),'*');
 % plot3(U(:,1),U(:,2),U(:,3),'*');
 % plot3(E(:,1),E(:,2),E(:,3),'*');
 % plot3(initial_pos(:,1),initial_pos(:,2),initial_pos(:,3),'*');
 % grid on;
 
+%% Draw trajectories:
+for i=1:1:6
+    mid(i,:)= [(T(i,1)+initial_pos(i,1))/2, (T(i,2)+initial_pos(i,2))/2, (T(i,3)+initial_pos(i,3))/2]; %middle point
+    traj_T(:,:,i)=[initial_pos(i,:);mid(i,:);T(i,:)];
+    mid_U(i,:)= [(T(i,1)+U(i,1))/2, (T(i,2)+U(i,2))/2, (T(i,3)+U(i,3))/2];
+    traj_U(:,:,i)=[T(i,:);mid_U(i,:);U(i,:)];
+    mid_E(i,:)= [(U(i,1)+E(i,1))/2, (U(i,2)+E(i,2))/2, (U(i,3)+E(i,3))/2];
+    traj_E(:,:,i)=[U(i,:);mid_E(i,:);E(i,:)];
+end
+mid_E(7,:)= [(initial_pos(7,1)+E(7,1))/2, (initial_pos(7,2)+E(7,2))/2, (initial_pos(7,3)+E(7,3))/2];
+traj_E(:,:,7)=[initial_pos(7,:);mid_E(7,:);E(7,:)];
+
+% Trajectories plot:
 figure(2);
 grid on;
 hold on;
-plot3(traj_T1(:,1),traj_T1(:,2),traj_T1(:,3),'*-',color='red');
-plot3(traj_T2(:,1),traj_T2(:,2),traj_T2(:,3),'*-',color='yellow');
-plot3(traj_T3(:,1),traj_T3(:,2),traj_T3(:,3),'*-',color='blue');
-plot3(traj_T4(:,1),traj_T4(:,2),traj_T4(:,3),'*-',color='green');
-plot3(traj_T5(:,1),traj_T5(:,2),traj_T5(:,3),'*-',color='cyan');
-plot3(traj_T6(:,1),traj_T6(:,2),traj_T6(:,3),'*-',color='magenta');
+% plot3(traj_T(:,1,1),traj_T(:,2,1),traj_T(:,3,1),'*-',color='red');
+% plot3(traj_T(:,1,2),traj_T(:,2,2),traj_T(:,3,2),'*-',color='yellow');
+% plot3(traj_T(:,1,3),traj_T(:,2,3),traj_T(:,3,3),'*-',color='blue');
+% plot3(traj_T(:,1,4),traj_T(:,2,4),traj_T(:,3,4),'*-',color='green');
+% plot3(traj_T(:,1,5),traj_T(:,2,5),traj_T(:,3,5),'*-',color='cyan');
+% plot3(traj_T(:,1,6),traj_T(:,2,6),traj_T(:,3,6),'*-',color='magenta');
 
-waypoints_orientation1 =zeros(size(traj_T1,1),3);
-waypoints_orientation2 =zeros(size(traj_T2,1),3);
-waypoints_orientation3 =zeros(size(traj_T3,1),3);
-waypoints_orientation4 =zeros(size(traj_T4,1),3);
-waypoints_orientation5 =zeros(size(traj_T5,1),3);
-waypoints_orientation6 =zeros(size(traj_T6,1),3);
+% plot3(traj_U(:,1,1),traj_U(:,2,1),traj_U(:,3,1),'*-',color='red');
+% plot3(traj_U(:,1,2),traj_U(:,2,2),traj_U(:,3,2),'*-',color='yellow');
+% plot3(traj_U(:,1,3),traj_U(:,2,3),traj_U(:,3,3),'*-',color='blue');
+% plot3(traj_U(:,1,4),traj_U(:,2,4),traj_U(:,3,4),'*-',color='green');
+% plot3(traj_U(:,1,5),traj_U(:,2,5),traj_U(:,3,5),'*-',color='cyan');
+% plot3(traj_U(:,1,6),traj_U(:,2,6),traj_U(:,3,6),'*-',color='magenta');
 
+plot3(traj_E(:,1,1),traj_E(:,2,1),traj_E(:,3,1),'*-',color='red');
+plot3(traj_E(:,1,2),traj_E(:,2,2),traj_E(:,3,2),'*-',color='yellow');
+plot3(traj_E(:,1,3),traj_E(:,2,3),traj_E(:,3,3),'*-',color='blue');
+plot3(traj_E(:,1,4),traj_E(:,2,4),traj_E(:,3,4),'*-',color='green');
+plot3(traj_E(:,1,5),traj_E(:,2,5),traj_E(:,3,5),'*-',color='cyan');
+plot3(traj_E(:,1,6),traj_E(:,2,6),traj_E(:,3,6),'*-',color='magenta');
+plot3(traj_E(:,1,7),traj_E(:,2,7),traj_E(:,3,7),'*-',color='black');
 
+for i=1:1:6
+    waypoints_orientation(:,:,i)=zeros(size(traj_T,1),3);
+end
 % Time_interval=[1.9273,1.2,0.5,0.7654,0.7654,0.7654,0.7654,0.7654,0.7654,0.5,1];     %Figure 6 time interval
 % Time_interval=[1.05,0.71,0.62,0.7,0.56,0.56,0.7,0.62,0.71,1.05,1.05];   %Figure 8 time interval
 Time_T = [3,3,3];
@@ -87,84 +115,52 @@ for i=1:length(Time_T)-1
     Time_of_Arrival(i+1)=Time_T(i+1)+sum(Time_T(1:i));
 end
 Time_of_Arrival(1)=Time_T(1);
-waypoints1=[traj_T1,waypoints_orientation1].';
-waypoints2=[traj_T2,waypoints_orientation2].';
-waypoints3=[traj_T3,waypoints_orientation3].';
-waypoints4=[traj_T4,waypoints_orientation4].';
-waypoints5=[traj_T5,waypoints_orientation5].';
-waypoints6=[traj_T6,waypoints_orientation6].';
+
+for i=1:1:6
+    waypoints(:,:,i)=[traj_T(:,:,i),waypoints_orientation(:,:,i)].';
+end
 
 %% Generate trajectory
 numsamples=100;
 % [q,qd,qdd,qddd,pp,timepoints,tsamples] = minjerkpolytraj(waypoints1,Time_of_Arrival,numsamples);
-[q1,qd1,qdd1,qddd1,pp1,timepoints1,tsamples1] = minjerkpolytraj(waypoints1,Time_of_Arrival,numsamples);
-[q2,qd2,qdd2,qddd2,pp2,timepoints2,tsamples2] = minjerkpolytraj(waypoints2,Time_of_Arrival,numsamples);
-[q3,qd3,qdd3,qddd3,pp3,timepoints3,tsamples3] = minjerkpolytraj(waypoints3,Time_of_Arrival,numsamples);
-[q4,qd4,qdd4,qddd4,pp4,timepoints4,tsamples4] = minjerkpolytraj(waypoints4,Time_of_Arrival,numsamples);
-[q5,qd5,qdd5,qddd5,pp5,timepoints5,tsamples5] = minjerkpolytraj(waypoints5,Time_of_Arrival,numsamples);
-[q6,qd6,qdd6,qddd6,pp6,timepoints6,tsamples6] = minjerkpolytraj(waypoints6,Time_of_Arrival,numsamples);
+for i=1:1:6
+    [q(:,:,i),qd(:,:,i),qdd(:,:,i),qddd(:,:,i),pp(:,:,i),timepoints(:,:,i),tsamples(:,:,i)] = minjerkpolytraj(waypoints(:,:,i),Time_of_Arrival,numsamples);
+end
 
 duration_time=Time_T;
-% position_coefficients_x1 =pp.coefs(1:6:end,8:-1:1) ;
-position_coefficients_x1 =pp1.coefs(1:6:end,8:-1:1) ;
-position_coefficients_y1 =pp1.coefs(2:6:end,8:-1:1) ;
-position_coefficients_z1 =pp1.coefs(3:6:end,8:-1:1) ;
-orientation_coefficients_yaw1=pp1.coefs(4:6:end,8:-1:1) ;
 
-position_coefficients_x2 =pp2.coefs(1:6:end,8:-1:1) ;
-position_coefficients_y2 =pp2.coefs(2:6:end,8:-1:1) ;
-position_coefficients_z2 =pp2.coefs(3:6:end,8:-1:1) ;
-orientation_coefficients_yaw2=pp2.coefs(4:6:end,8:-1:1) ;
+for i=1:1:6
+    position_coefficients_x(:,:,i) =pp(:,:,i).coefs(1:6:end,8:-1:1) ;
+    position_coefficients_y(:,:,i) =pp(:,:,i).coefs(2:6:end,8:-1:1) ;
+    position_coefficients_z(:,:,i) =pp(:,:,i).coefs(3:6:end,8:-1:1) ;
+    orientation_coefficients_yaw(:,:,i)=pp(:,:,i).coefs(4:6:end,8:-1:1) ;
+end
 
-position_coefficients_x3 =pp3.coefs(1:6:end,8:-1:1) ;
-position_coefficients_y3 =pp3.coefs(2:6:end,8:-1:1) ;
-position_coefficients_z3 =pp3.coefs(3:6:end,8:-1:1) ;
-orientation_coefficients_yaw3=pp3.coefs(4:6:end,8:-1:1) ;
-
-position_coefficients_x4 =pp4.coefs(1:6:end,8:-1:1) ;
-position_coefficients_y4 =pp4.coefs(2:6:end,8:-1:1) ;
-position_coefficients_z4 =pp4.coefs(3:6:end,8:-1:1) ;
-orientation_coefficients_yaw4=pp4.coefs(4:6:end,8:-1:1) ;
-
-position_coefficients_x5 =pp5.coefs(1:6:end,8:-1:1) ;
-position_coefficients_y5 =pp5.coefs(2:6:end,8:-1:1) ;
-position_coefficients_z5 =pp5.coefs(3:6:end,8:-1:1) ;
-orientation_coefficients_yaw5=pp5.coefs(4:6:end,8:-1:1) ;
-
-position_coefficients_x6 =pp6.coefs(1:6:end,8:-1:1) ;
-position_coefficients_y6 =pp6.coefs(2:6:end,8:-1:1) ;
-position_coefficients_z6 =pp6.coefs(3:6:end,8:-1:1) ;
-orientation_coefficients_yaw6=pp6.coefs(4:6:end,8:-1:1) ;
-
-figure(3)
-hold on;
-plot3(q1(1,:),q1(2,:),q1(3,:),'*');
-plot3(q2(1,:),q2(2,:),q2(3,:),'*');
-plot3(q3(1,:),q3(2,:),q3(3,:),'*');
-plot3(q4(1,:),q4(2,:),q4(3,:),'*');
-plot3(q5(1,:),q5(2,:),q5(3,:),'*');
-plot3(q6(1,:),q6(2,:),q6(3,:),'*');
-% plot3(traj_T1(:,1),traj_T1(:,2),traj_T1(:,3),'r*-');
-
-grid on
+% figure(3)
+% hold on
+% plot3(q(1,:,1),q(2,:,1),q(3,:,1),'*');
+% plot3(q(1,:,2),q(2,:,2),q(3,:,2),'*');
+% plot3(q(1,:,3),q(2,:,3),q(3,:,3),'*');
+% plot3(q(1,:,4),q(2,:,4),q(3,:,4),'*');
+% plot3(q(1,:,5),q(2,:,5),q(3,:,5),'*');
+% plot3(q(1,:,6),q(2,:,6),q(3,:,6),'*');
+% grid on
 
 %% Save coefficients
-% fid = fopen( 'T_1.csv', 'w' );
-Coeffients1=[duration_time(1:end-1).',position_coefficients_x1,position_coefficients_y1,position_coefficients_z1,orientation_coefficients_yaw1];
+Coeffients1=[duration_time(1:end-1).',position_coefficients_x(:,:,1),position_coefficients_y(:,:,1),position_coefficients_z(:,:,1),orientation_coefficients_yaw(:,:,1)];
 Create_coefficients_table(Coeffients1,'T_1.csv');
 
-Coeffients2=[duration_time(1:end-1).',position_coefficients_x2,position_coefficients_y2,position_coefficients_z2,orientation_coefficients_yaw2];
+Coeffients2=[duration_time(1:end-1).',position_coefficients_x(:,:,2),position_coefficients_y(:,:,2),position_coefficients_z(:,:,2),orientation_coefficients_yaw(:,:,2)];
 Create_coefficients_table(Coeffients2,'T_2.csv');
 
-Coeffients3=[duration_time(1:end-1).',position_coefficients_x3,position_coefficients_y3,position_coefficients_z3,orientation_coefficients_yaw3];
+Coeffients3=[duration_time(1:end-1).',position_coefficients_x(:,:,3),position_coefficients_y(:,:,3),position_coefficients_z(:,:,3),orientation_coefficients_yaw(:,:,3)];
 Create_coefficients_table(Coeffients3,'T_3.csv');
 
-Coeffients4=[duration_time(1:end-1).',position_coefficients_x4,position_coefficients_y4,position_coefficients_z4,orientation_coefficients_yaw4];
+Coeffients4=[duration_time(1:end-1).',position_coefficients_x(:,:,4),position_coefficients_y(:,:,4),position_coefficients_z(:,:,4),orientation_coefficients_yaw(:,:,4)];
 Create_coefficients_table(Coeffients4,'T_4.csv');
 
-Coeffients5=[duration_time(1:end-1).',position_coefficients_x5,position_coefficients_y5,position_coefficients_z5,orientation_coefficients_yaw5];
+Coeffients5=[duration_time(1:end-1).',position_coefficients_x(:,:,5),position_coefficients_y(:,:,5),position_coefficients_z(:,:,5),orientation_coefficients_yaw(:,:,5)];
 Create_coefficients_table(Coeffients5,'T_5.csv');
 
-Coeffients6=[duration_time(1:end-1).',position_coefficients_x6,position_coefficients_y6,position_coefficients_z6,orientation_coefficients_yaw6];
+Coeffients6=[duration_time(1:end-1).',position_coefficients_x(:,:,6),position_coefficients_y(:,:,6),position_coefficients_z(:,:,6),orientation_coefficients_yaw(:,:,6)];
 Create_coefficients_table(Coeffients6,'T_6.csv');
-
