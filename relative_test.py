7911c177f370083e400f5a09132ef1aa302bbeae
marci0000 <164386734+marci0000@users.noreply.github.com>
2024-05-27 18:15:34 +0200

    Add files via upload

---
+++ relative_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from pycrazyswarm import *
+import uav_trajectory
+
+if __name__ == "__main__":
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+
+    traj1 = uav_trajectory.Trajectory()
+    traj1.loadcsv("wave_1.csv")
+
+    #TRIALS = 1
+    TRIALS = 1
+    TIMESCALE = 1.0
+    for i in range(TRIALS):
+        for cf in allcfs.crazyflies:
+            cf.uploadTrajectory(0, 0, traj1)
+
+        allcfs.takeoff(targetHeight=1.0, duration=2.0)
+        timeHelper.sleep(2.5)
+
+        allcfs.startTrajectory(0, timescale=TIMESCALE, relative = True)
+        timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
+        
+        # allcfs.startTrajectory(0, timescale=TIMESCALE, reverse=True)
+        # timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
+
+        allcfs.land(targetHeight=0.06, duration=2.0)
+        timeHelper.sleep(3.0)
+
