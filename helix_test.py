c3997fd3610a9447bb480f6b8481a1ea1115fbcb
marci0000 <164386734+marci0000@users.noreply.github.com>
2024-05-31 19:27:40 +0200

    Update helix_test.py

--- helix_test.py
+++ helix_test.py
@@ -11,23 +11,22 @@ if __name__ == "__main__":
     allcfs = swarm.allcfs
 
     traj1 = uav_trajectory.Trajectory()
-    traj1.loadcsv("figure8_fast.csv")
+    traj1.loadcsv("helix1.csv")
 
     #TRIALS = 1
     TRIALS = 1
     TIMESCALE = 1.0
     for i in range(TRIALS):
         for cf in allcfs.crazyflies:
-            cf.uploadTrajectory(0, 0, traj1)
+            cf.uploadTrajectory(0, 0, helix1)
 
-        allcfs.takeoff(targetHeight=1.0, duration=2.0)
-        timeHelper.sleep(2.5)
-        for cf in allcfs.crazyflies:
-            pos = np.array(cf.initialPosition) + np.array([0, 0, 1.0])
-            cf.goTo(pos, 0, 2.0)
+        allcfs.takeoff(targetHeight=0.2, duration=2.0)
         timeHelper.sleep(2.5)
 
-        allcfs.startTrajectory(0, timescale=TIMESCALE)
+        allcfs.startTrajectory(0, timescale=TIMESCALE, relative = True)
+        timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
+
+        allcfs.startTrajectory(0, timescale=TIMESCALE, reverse = True, relative = True)
         timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
         
         # allcfs.startTrajectory(0, timescale=TIMESCALE, reverse=True)
